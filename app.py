2a5e21fedb4afd63eb6f2c4320b88b2e90951593
Daniel Crawford <danielcrawford@Daniels-MacBook-Pro.local>
2025-04-08 15:55:21 -0400

    add secrets

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
 import plotly.express as px
 from PIL import Image
 
@@ -41,7 +39,7 @@ def authenticate():
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
         if st.button("Login"):
-            if username == os.environ['username'] and password == os.environ['password'] :
+            if username == st.secrets['username'] and password == st.secrets['password'] :
                 st.session_state.authenticated = True
             else:
                 st.error("Invalid credentials")
