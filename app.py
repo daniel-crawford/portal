84faf82f12e38d7c7d5c465adcc3e889bdb4f977
Daniel Crawford <danielcrawford@Daniels-MacBook-Pro.local>
2025-04-08 15:44:57 -0400

    hide pw 2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 from utils.plot import generate_dummy_data, plot_usage, plot_cost
 
-load_dotenv()
+load_dotenv(".pwenv")
 
 # ============================================
 # Constants and Config
